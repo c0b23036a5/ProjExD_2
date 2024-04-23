e5fe89750714d97c83c4fb544c89bd58fd11d9da
Nobunagay <c0b23036a5@edu.teu.ac.jp>
2024-04-23 15:19:55 +0900

    練習問題3　終了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -47,6 +47,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bk_rct):
+            print("Game Over")
+            return
         screen.blit(bg_img, [0, 0]) 
          # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
